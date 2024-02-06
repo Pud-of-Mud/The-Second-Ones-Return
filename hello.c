e6b5187f7112d799c4de2c7d89a0380eda36ee5a
Pud-of-Mud <109466200+Pud-of-Mud@users.noreply.github.com>
2024-02-06 15:00:42 -0500

    Revamped menu

--- hello.c
+++ hello.c
@@ -15,18 +15,26 @@ Finally, turn on the PPU to display video.
 void main(void) {
 
   // set palette colors
-  pal_col(0,0x02);	// set screen to dark blue
+  pal_col(0,0x00);	// dark grey
   pal_col(1,0x14);	// fuchsia
   pal_col(2,0x20);	// grey
   pal_col(3,0x30);	// white
 
   // write text to name table
-  vram_adr(NTADR_A(2,2));		// set address
-  vram_write("HELLO, WORLD!", 13);	// write bytes to video RAM
-
+  vram_adr(NTADR_A(10,10));	// set address
+  vram_write("",10);	// write bytes to video RAM
+  
+  vram_adr(NTADR_A(8,20));
+  vram_write("Press A to start", 16);
+  
   // enable PPU rendering (turn on screen)
   ppu_on_all();
 
   // infinite loop
-  while (1) ;
+  while (1) {
+//    if () { // Check for 'A' input
+//      // Clear Screen to start the game
+//      
+//    }
+  };
 }
